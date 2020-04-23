2252942d80820de0eceec5a4c804f56ea89c6748
Zach Schuster <ztschuster@gmail.com>
2020-04-23 08:28:11 -0400

    basic navigation

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screen2.dart';
 
 class Screen1 extends StatelessWidget {
   @override
@@ -12,7 +13,11 @@ class Screen1 extends StatelessWidget {
         child: RaisedButton(
           color: Colors.red,
           child: Text('Go Forwards To Screen 2'),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return Screen2();
+            }));
+          },
         ),
       ),
     );
